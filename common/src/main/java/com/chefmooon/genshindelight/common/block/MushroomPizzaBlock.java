9f9cff3e7ba7e330fda0564886fa998feace17bb
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-05-26 10:12:50 -0400

    feat: mushroom pizza block custom hitbox

--- common/src/main/java/com/chefmooon/genshindelight/common/block/MushroomPizzaBlock.java
+++ common/src/main/java/com/chefmooon/genshindelight/common/block/MushroomPizzaBlock.java
@@ -1,14 +1,38 @@
 package com.chefmooon.genshindelight.common.block;
 
 import com.chefmooon.genshindelight.common.block.base.BaseFeastBlock;
+import com.chefmooon.genshindelight.common.registry.ModShapes;
+import com.chefmooon.genshindelight.common.util.VoxelShapeUtil;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 public class MushroomPizzaBlock extends BaseFeastBlock {
-
-    // TODO: add custom hitbox
+    protected final ConcurrentHashMap<Integer, VoxelShape[]> SHAPES;
     public MushroomPizzaBlock(Properties properties, Supplier<Item> servingItem) {
         super(properties, servingItem);
+        SHAPES = buildShapes();
+    }
+
+    private static ConcurrentHashMap<Integer, VoxelShape[]> buildShapes() {
+        ConcurrentHashMap<Integer, VoxelShape[]> result = new ConcurrentHashMap<>();
+        result.put(4, VoxelShapeUtil.getRotatedShapes(ModShapes.MUSHROOM_PIZZA_SERVINGS_4));
+        result.put(3, VoxelShapeUtil.getRotatedShapes(ModShapes.MUSHROOM_PIZZA_SERVINGS_3));
+        result.put(2, VoxelShapeUtil.getRotatedShapes(ModShapes.MUSHROOM_PIZZA_SERVINGS_2));
+        result.put(1, VoxelShapeUtil.getRotatedShapes(ModShapes.MUSHROOM_PIZZA_SERVINGS_1));
+        result.put(0, VoxelShapeUtil.getRotatedShapes(ModShapes.BASIC_PLATE));
+        return result;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        int servings = state.getValue(getServingsProperty());
+        return SHAPES.containsKey(servings) ? SHAPES.get(servings)[state.getValue(FACING).get2DDataValue()] : ModShapes.BASIC_PLATE;
     }
 }
--- common/src/main/java/com/chefmooon/genshindelight/common/registry/ModShapes.java
+++ common/src/main/java/com/chefmooon/genshindelight/common/registry/ModShapes.java
@@ -22,4 +22,21 @@ public class ModShapes {
     public static final VoxelShape TEA_BREAK_PANCAKE_SERVINGS_1 = Shapes.or(
             BASIC_PLATE, Block.box(2, 1, 2, 14, 2, 14)
     );
+
+    private static final VoxelShape MUSHROOM_PIZZA_1 = Block.box(8, 1, 2, 14, 3, 8);
+    private static final VoxelShape MUSHROOM_PIZZA_2 = Block.box(2, 1, 2, 8, 3, 8);
+    private static final VoxelShape MUSHROOM_PIZZA_3 = Block.box(2, 1, 8, 8, 3, 14);
+    private static final VoxelShape MUSHROOM_PIZZA_4 = Block.box(8, 1, 8, 14, 3, 14);
+    public static final VoxelShape MUSHROOM_PIZZA_SERVINGS_4 = Shapes.or(
+            BASIC_PLATE, MUSHROOM_PIZZA_1, MUSHROOM_PIZZA_2, MUSHROOM_PIZZA_3, MUSHROOM_PIZZA_4
+    );
+    public static final VoxelShape MUSHROOM_PIZZA_SERVINGS_3 = Shapes.or(
+            BASIC_PLATE, MUSHROOM_PIZZA_1, MUSHROOM_PIZZA_2, MUSHROOM_PIZZA_3
+    );
+    public static final VoxelShape MUSHROOM_PIZZA_SERVINGS_2 = Shapes.or(
+            BASIC_PLATE, MUSHROOM_PIZZA_1, MUSHROOM_PIZZA_2
+    );
+    public static final VoxelShape MUSHROOM_PIZZA_SERVINGS_1 = Shapes.or(
+            BASIC_PLATE, MUSHROOM_PIZZA_1
+    );
 }
